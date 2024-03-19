a4dc0cb6e7ee52fad24f6c40271bb3d54cb5f35f
AliSiddique <alisiddique10@hotmail.com>
2024-03-19 21:06:59 +0000

    AD

---
+++ config.ts
@@ -0,0 +1,33 @@
+
+
+
+const config = {
+    name: 'config',
+    siteUrl: 'https://www.example.com',
+    title: 'Example',
+    description: 'Example description',
+    lang: 'en',
+    locale: 'en_US',
+    fbAppId: '123456789',
+    twitterHandle: '@example',
+    themeColor: '#000000',
+    backgroundColor: '#ffffff',
+    favicon: '/favicon.ico',
+    shortName: 'Example',
+    startUrl: '/',
+    display: 'standalone',
+    icon: '/icon.png',
+    maskableIcon: '/maskable_icon.png',
+    msTileImage: '/ms_tile_image.png',
+    appleMaskIcon: '/apple_mask_icon.png',
+    gcmSenderId: '123456789',
+    vapidPublicKey: 'VAPID_PUBLIC_KEY',
+    googleAnalyticsId: 'UA-123456789-1',
+    gtmId: 'GTM-123456789',
+    gtmAuth: 'GTM-123456789',
+    gtmPreview: 'env-1',
+    gtmCookiesWin: 'x',
+    gtmCookies: 'x',
+    gtmPreviewEnv: '1',
+    
+}
