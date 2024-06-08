ac632c81600a6373e436002e42990a0778f7be5e
AliSiddique <alisiddique10@hotmail.com>
2024-06-08 14:41:53 +0100

    AS

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,66 +1,7 @@
-// import Footer from "@/components/landing-pages/light-saas/Footer";
-// import Navbar from "@/components/landing-pages/light-saas/Navbar";
-// import { getAllPosts } from "@/lib/mdx";
-// import Link from "next/link";
-// import React from "react";
-
-// type Props = {};
-
-// export default async function page({}: Props) {
-//   const posts = await getAllPosts();
-
-//   return (
-//     <div>
-//       <Navbar />
-//       <div className="-m-4 flex flex-wrap mx-auto max-w-7xl py-5">
-//         {posts.map((blog: any) => (
-//           <div key={blog.meta.slug} className="p-4 md:w-1/3">
-//             <div className="h-full overflow-hidden rounded-lg border-2 border-gray-200 border-opacity-60">
-//               <img
-//                 className="w-full object-cover object-center md:h-36 lg:h-48"
-//                 src={blog.meta.featuredimage}
-//                 alt="blog"
-//               />
-//               <div className="p-6">
-//                 <h1 className="title-font mb-3 text-lg font-medium text-gray-700 dark:text-white">
-//                   {blog.meta.title}
-//                 </h1>
-//                 <p className="mb-3 leading-relaxed text-gray-700 dark:text-white">
-//                   {blog.meta.description}
-//                 </p>
-//                 <div className="flex flex-wrap items-center ">
-//                   <Link
-//                     href={`/blog/${blog.meta.slug}`}
-//                     className="inline-flex items-center text-green-500 md:mb-2 lg:mb-0"
-//                   >
-//                     Read
-//                     <svg
-//                       className="ml-2 h-4 w-4"
-//                       viewBox="0 0 24 24"
-//                       stroke="currentColor"
-//                       strokeWidth="2"
-//                       fill="none"
-//                       strokeLinecap="round"
-//                       strokeLinejoin="round"
-//                     >
-//                       <path d="M5 12h14"></path>
-//                       <path d="M12 5l7 7-7 7"></path>
-//                     </svg>
-//                   </Link>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
-
 import React from 'react'
 import { Author, Post, allAuthors, allPosts } from "contentlayer/generated";
 import Link from 'next/link';
+import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 type Props = {}
 
 export const revalidate = 10;
@@ -77,11 +18,27 @@ export default function page({}: Props) {
     <div>
       {posts.map((post) => (
         <div>
-          <Link href={`/blog/${post.slug}`}>
-          <h1 className='text-black'>{post.title}</h1>
-          </Link>
+
+          <Card className="bg-white dark:bg-black border-gray-300 px-0 mt-5">
+            <CardHeader>{post.author?.name}</CardHeader>
+            <CardContent>
+              <div className="flex flex-col space-y-4">
+                <p className="text-sm text-muted-foreground">
+                  {post.brief}
+                </p>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <div className="flex justify-end">
+                <Link href={`/blog/${post.slug}`}>
+                  Read
+                </Link>
+              </div>
+            </CardFooter>
+          </Card>
         </div>
       ))}
+
     </div>
   )
 }
